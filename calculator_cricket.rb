e1fe83f4bf2f6ce783609d88699b21e0811edb64
Hemesh Unka <hemesh@Hemeshs-MacBook-Air.local>
2017-12-19 14:48:16 +0000

    Clean up code

--- calculator_cricket.rb
+++ calculator_cricket.rb
@@ -1,7 +1,3 @@
-## Two players
-## Determines who is winner at end of the game by comparing totals
-## may even evolve the program to include batsmen scores
-
 class Player
   def initialize(name, number, runs = 0)
     @player_name = "#{name} #{number}"
@@ -17,31 +13,32 @@ class Player
   end
 end
 
-class Team
-  def initialize(name)
-    @player_assigned_name = name
-    @total_runs = 0
+class Team(runs = 0)
+  attr_accessor :owner_name
+
+  def initialize
+    @owner_name = ""
+    @total_runs = runs
+    @players = []
   end
 
   def create_team
-    team = {}
-    (1..11).each { |x| team["player" + x.to_s] = create_player(x) }
-    return team
+    (1..11).each { |x| @players << create_player(x) }
   end
 
-  def create_player(num)
-    Player.new(@player_assigned_name, num)
+  def create_player(number)
+    Player.new(@owner_name, number)
   end
 
   def total_team_runs(team)
-    @total_runs
+    @total_runs = @players.reduce(:+)
   end
 end
 
 class Game
   def initialize
-    @p1_name = ""
-    @p2_name = ""
+    @p1 = Team.new
+    @p2 = Team.new
   end
 
   def start_up
@@ -50,50 +47,58 @@ class Game
     puts "Welcome to Calculator Cricket!"
 
     # enter names
-    @p1_name = input("Player 1 - Please enter your name").capitalize
-    @p2_name = input("Player 2 - Please enter your name").capitalize
+    @p1.owner_name = input("Player 1 - Please enter your name").capitalize
+    @p2.owner_name = input("Player 2 - Please enter your name").capitalize
 
-    # toss
     # one person is randomly selected for the toss
-    decides_toss = random([@p1_name, @p2_name])
+    @toss_winner = random([@p1, @p2])
+    # that person then chooses heads or tails
+    loop do
+      @toss_winner_decision = input("#{@toss_winner.owner_name} - gets to decide the toss. Please enter \"heads\" or \"tails\" to continue.").downcase
+      break if @toss_winner_decision == "heads" || @toss_winner_decision == "tails"
+    end
+    #begin computer toss
+    @computer_toss = random(["heads", "tails"])
+    puts ".....and it is #{@computer_toss}."
+    # if the call is the same as the toss then the one person gets to decide wheter to bat or bowl
+
+    # @toss_winner = @p1
+    # @toss_winner_decision = "Heads"
+    # @computer_toss = "Heads"
+    # @toss_winner = @p1
+    # @toss_looser = @p2
+    # @toss_winner_decision = "Bat"
+    # @toss_looser_decision = "Bowl"
+
+    # else the other person gets to decide
+
+
+
 
     # that person gets to choose if it is heads or tails
-    loop do
-      @player_toss_decision = input("#{decides_toss} - gets to decide the toss. Please enter \"heads\" or \"tails\" to continue.").downcase
-      break if @player_toss_decision == "heads" || @player_toss_decision == "tails"
-    end
 
-    # now the computer gets to randomly choose heads or tails
-    computer_toss = random(["heads", "tails"])
-    puts ".....and it is #{computer_toss}."
+
+    # computer randomly chooses heads or tails
 
     # logic to decide looser of toss
-    toss_looser = decides_toss == @p1_name ? @p2_name : @p1_name
+    @toss_looser = @toss_winner.owner_name == @p1.owner_name ? @p2 : @p1
 
     loop do
-      print computer_toss == @player_toss_decision ? "#{decides_toss} - Wins the toss." : "#{decides_toss} - Lost the toss! #{toss_looser}"
+      print @computer_toss == @toss_decision ? "#{@toss_winner.owner_name} - Won the toss." : "#{@toss_winner.owner_name} - Lost the toss! #{@toss_looser.owner_name}"
       @toss_winner_decision = input(" - Please enter either \"bat\" or \"bowl\" to continue.").downcase
-      @toss_outcome = [[decides_toss, toss_looser], @toss_winner_decision]
       break if @toss_winner_decision == "bat" || @toss_winner_decision == "bowl"
-      end
+    end
+
+    # Find out who is batting first
+
+
     main_game
   end
 
-def input(string)
-  puts string
-  print "> "
-  return gets.chomp
-end
-
 def main_game
-  # create new teams
-  team = Team.new(@toss_outcome[0][0])
-  team_1 = team.create_team
 
-  team = Team.new(@toss_outcome[0][1])
-  team_2 = team.create_team
 
-  innings(team_1)
+
 end
 
 def ball
@@ -104,28 +109,15 @@ def over
   (0...6).to_a.map { ball }
 end
 
-def innings(team)
-  on_strike = team[0]
-  non_striker = team[1]
-
-  #if odd then other player on on_strike
-
-
-
-  puts team.inspect
-  # batsmen_1 = team.player1
-  # batsmen_2 = team.player2
-
-
+def bat(team)
   wickets = 0;
-  total_runs = 0;
-    print "["
 
   (0...50).each do
     over.each do |y|
 
       if y == 0
         print "OUT!"
+        wickets += 1
       elsif y == 4
         print"4 Runs"
       elsif y == 6
@@ -135,20 +127,22 @@ def innings(team)
       end
         print " - "
 
-      total_runs += y
       break if wickets == 11
-      wickets += 1 if y == 0
     end
   end
-  print "]"
-
-  return [wickets, total_runs]
-end
 
 end
 
-def random(array)
-  array.sample
+  def input(string)
+    puts string
+    print "> "
+    return gets.chomp
+  end
+
+  def random(array)
+    array.sample
+  end
+
 end
 
 def clear_screen
